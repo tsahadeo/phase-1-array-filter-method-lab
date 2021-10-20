a2fb48af89a8f263bf974443c9aeedd11e27d757
Travis <tsahadeo@mac.com>
2021-10-20 12:43:42 -0400

    commit tree 31c4f6268a396edd06adb17418a0c159bbdfbe34

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(array, name){
+    let answer = array.filter(function (person) {
+        return person.toLowerCase() === name.toLowerCase()
+    });
+    return answer;
+}
+
+function fuzzyMatch(array, string){
+    let kindaMatch = array.filter(function(person){
+        return person.slice(0, 2) === string
+    });
+    return kindaMatch
+}
+
+function matchName(array, string){
+return array.filter(function(driver){
+    return driver.name === string
+}); 
+}
